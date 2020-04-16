05bed2e4f77966007350202dbe53558e1a08642d
TikaYulian27 <tikayulian27@gmail.com>
2020-04-16 12:50:52 +0700

    add firebase.config.js

---
+++ modul10/src/firebase.config.js
@@ -0,0 +1,10 @@
+export default{
+    apiKey: "AIzaSyAjoKv48BUB6Ty9K8uCu8xPikKTjuJmYR8",
+    authDomain: "pbfmodul10.firebaseapp.com",
+    databaseURL: "https://pbfmodul10.firebaseio.com",
+    projectId: "pbfmodul10",
+    storageBucket: "pbfmodul10.appspot.com",
+    messagingSenderId: "434602694382",
+    appId: "1:434602694382:web:4135eced8a547f148945ff",
+    measurementId: "G-EJGT0D6J4F"
+}
